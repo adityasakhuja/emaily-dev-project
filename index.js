de5f7ef65186377d2f511caa5a987f2aeefdda52
Aditya Sakhuja <aditya.sakhuja@live.com>
2019-06-06 07:37:07 +0530

    mongoose setup

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 // no const required as we're not pulling anything out of passport.js file
+require("./models/User");
 require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
 // Long method:
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users"); // one arg means pulling, 2 means adding
 
 // Set up Passport Google OAuth Strategy
 passport.use(
@@ -11,9 +14,7 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile:", profile);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
